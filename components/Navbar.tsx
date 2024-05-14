9d52633aae9becf6227b4456745f220d32ed78d3
Edward Kim <itgma1231@gmail.com>
2024-05-13 23:19:34 -0400

    Navbar.tsx - <nav> styling/pepe logo added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className='flex justify-between max-container padding-container relative z-30 py-5'>
+      <Link href='/'>
+        <Image src='/pepe.svg' alt='Pepe Logo' width={70} height={60} />
+      </Link>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
