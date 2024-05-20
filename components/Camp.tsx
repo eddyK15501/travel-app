99e273b14de377dd6946acb5957578b2e683d9bf
Edward Kim <itgma1231@gmail.com>
2024-05-20 19:56:40 -0400

    Camp.tsx - initial start; tailwindcss responsive containers added

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Camp = () => {
   return (
-    <div>Camp</div>
-  )
-}
+    <section className='2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20'>
+      <div className='hide-scrollbar flex h-[340px] w-full justify-start items-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]'></div>
+    </section>
+  );
+};
 
-export default Camp
+export default Camp;
