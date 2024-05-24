a1128d8437ff73a1a186178c6e9eabea304aa3ad
Edward Kim <itgma1231@gmail.com>
2024-05-23 22:56:27 -0400

    Camp.tsx - caption banner position relative to <CampSite />

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -62,10 +62,23 @@ const Camp = () => {
         />
         <CampSite
           bgImage='bg-bg-img-2'
-          title='Dogwifhat Camp'
-          subtitle='Somewhere in the Woods'
+          title='Dogwifhat Mountain'
+          subtitle='Somewhere in Nature'
           peopleJoined='300+ joined'
         />
+      </div>
+      <div className='flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6'>
+        <div className='bg-green-50 relative w-full overflow-hidden rounded-3xl p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20'>
+          <h2 className='capitalize text-white regular-24 md:regular-32 2xl:regular-64'>
+            <strong>Feeling Lost</strong> And Don't Know The Way?
+          </h2>
+          <p className='regular-14 xl:regular-16 mt-5 text-white'>
+            While navigating the ups and downs of the memecoin market can be
+            challenging, finding solace in nature's embrace through a tranquil
+            hike can provide the perfect opportunity to reflect, recharge, and
+            gain perspective.
+          </p>
+        </div>
       </div>
     </section>
   );
