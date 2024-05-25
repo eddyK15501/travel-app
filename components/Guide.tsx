161b52f66b9f6ee25f820e706628c4f62eb382b9
Edward Kim <itgma1231@gmail.com>
2024-05-24 22:21:16 -0400

    Guide.tsx - start; section heading/icon added

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Image from 'next/image';
 
 const Guide = () => {
   return (
-    <div>Guide</div>
-  )
-}
+    <section className='flexCenter flex-col'>
+      <div className='w-full pb-24 padding-container max-container'>
+        <Image src='/camp.svg' alt='camp-icon' width={50} height={50} />
+        <p className='uppercase regular-18 -mt-1 mb-3 text-green-50'>
+          We Are Here For You
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default Guide
+export default Guide;
