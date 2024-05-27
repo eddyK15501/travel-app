f8bf0f7d751ec9958b1efcc4ca0590740b716f06
Edward Kim <itgma1231@gmail.com>
2024-05-26 21:34:05 -0400

    <FeatureItem /> - render map icons

--- components/Features.tsx
+++ components/Features.tsx
@@ -47,10 +47,21 @@ type FeatureItemProps = {
   icon: string;
   variant: string;
   description: string;
-}
+};
 
-const FeatureItem = ({ title, icon, variant, description }: FeatureItemProps) => {
-  return <div>{title}</div>;
+const FeatureItem = ({
+  title,
+  icon,
+  variant,
+  description,
+}: FeatureItemProps) => {
+  return (
+    <li className='flex w-full flex-1 flex-col items-start'>
+      <div className='rounded-full p-4 lg:p-7 bg-green-50'>
+        <Image src={icon} alt='map-icon' width={28} height={28} />
+      </div>
+    </li>
+  );
 };
 
 export default Features;
