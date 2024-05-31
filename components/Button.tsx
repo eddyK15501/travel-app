476aaf5064ed759722719aef1f2dc7b2e65820fd
Edward Kim <itgma1231@gmail.com>
2024-05-30 23:14:13 -0400

    Download.tsx complete/Footer.tsx start

--- components/Button.tsx
+++ components/Button.tsx
@@ -12,12 +12,12 @@ const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={`flex justify-center gap-3 rounded-full border ${variant} ${
+      className={`flex justify-center items-center gap-3 rounded-full border ${variant} ${
         full && 'w-full'
       }`}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <p className='whitespace-nowrap'>{title}</p>
+      <p className='whitespace-nowrap cursor-pointer'>{title}</p>
     </button>
   );
 };
