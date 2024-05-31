83192760bd0620fc07a3031d53c1e2e644356ff2
Edward Kim <itgma1231@gmail.com>
2024-05-30 23:07:36 -0400

    Download.tsx - iOS/android download buttons added

--- components/Download.tsx
+++ components/Download.tsx
@@ -5,10 +5,27 @@ const Download = () => {
     <section className='flexCenter w-full flex-col pb-[100px]'>
       <div className='get-app'>
         <div className='z-20 flex w-full flex-1 flex-col items-start justify-center gap-12'>
-          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Download the App Now!</h2>
-          <p className='regular-16 text-gray-10'>Available on iOS and Android. Check it out.</p>
+          <h2 className='bold-40 lg:bold-64 xl:max-w-[320px]'>
+            Download the App Now!
+          </h2>
+          <p className='regular-16 text-gray-10'>
+            Available on iOS and Android. Check it out.
+          </p>
           <div>
-            Button
+            <Button
+              type='button'
+              title='Download App'
+              icon='/apple.svg'
+              variant='btn_white'
+              full
+            />
+            <Button
+              type='button'
+              title='Download App'
+              icon='/android.svg'
+              variant='btn_dark_green_outline'
+              full
+            />
           </div>
         </div>
       </div>
