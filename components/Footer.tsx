fe19a89630cb2d1f6006b71626575a7385e6b062
Edward Kim <itgma1231@gmail.com>
2024-06-03 21:19:18 -0400

    Footer.tsx - main container/display flex

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='flexCenter mb-24'>
+      <div className='padding-container max-container flex w-full flex-col gap-14'></div>
+    </footer>
   )
 }
 
