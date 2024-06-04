86e09343e3726b0859b6b7f951b30f01353b6079
Edward Kim <itgma1231@gmail.com>
2024-06-03 21:30:49 -0400

    Footer.tsx - footer logo added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image"
+import Link from "next/link"
+
 const Footer = () => {
   return (
     <footer className='flexCenter mb-24'>
-      <div className='padding-container max-container flex w-full flex-col gap-14'></div>
+      <div className='padding-container max-container flex w-full flex-col gap-14'>
+        <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
+          <Link href='/' className='mb-10'>
+            <Image src='pepe.svg' alt='Pepe' width={55} height={55} />
+          </Link>
+        </div>
+      </div>
     </footer>
   )
 }
