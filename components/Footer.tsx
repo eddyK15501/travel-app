823bdc309ce10f0aae881bfc67baab26fc3a878a
Edward Kim <itgma1231@gmail.com>
2024-06-04 20:44:22 -0400

    Footer.tsx - .map() and render component section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image"
 import Link from "next/link"
 
+import { FOOTER_LINKS } from "@/constants"
+
 const Footer = () => {
   return (
     <footer className='flexCenter mb-24'>
@@ -9,10 +11,17 @@ const Footer = () => {
           <Link href='/' className='mb-10'>
             <Image src='pepe.svg' alt='Pepe' width={55} height={55} />
           </Link>
+          <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
+            {FOOTER_LINKS.map((col) => (
+              <FooterCol />
+            ))}
+          </div>
         </div>
       </div>
     </footer>
   )
 }
+
+const FooterCol = () => {}
 
 export default Footer
