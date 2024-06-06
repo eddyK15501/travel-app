84c1709d40a1a59e7757df21fe11687fa2fb7f45
Edward Kim <itgma1231@gmail.com>
2024-06-05 22:09:25 -0400

    Footer.tsx - mock social links added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { FOOTER_LINKS, FOOTER_CONTACT_INFO } from '@/constants';
+import { FOOTER_LINKS, FOOTER_CONTACT_INFO, SOCIALS } from '@/constants';
 
 const Footer = () => {
   return (
@@ -34,13 +34,26 @@ const Footer = () => {
                       className='flex gap-4 md:flex-col lg:flex-row'
                       key={link.label}
                     >
-                      <p className='whitespace-nowrap'>{link.label}:</p>
-                      <p className='medium-14 whitespace-nowrap text-blue-70'>
+                      <p className='regular-14 text-gray-30 whitespace-nowrap'>
+                        {link.label}:
+                      </p>
+                      <p className='regular-14 whitespace-nowrap text-gray-30'>
                         {link.value}
                       </p>
                     </Link>
                   );
                 })}
+              </FooterCol>
+            </div>
+            <div className='flex flex-col gap-5'>
+              <FooterCol title={SOCIALS.title}>
+                <ul className='regular-14 flex gap-4 text-gray-30'>
+                  {SOCIALS.links.map((link) => (
+                    <Link href='/' key={link}>
+                      <Image src={link} alt='logo' width={24} height={24} />
+                    </Link>
+                  ))}
+                </ul>
               </FooterCol>
             </div>
           </div>
@@ -58,7 +71,7 @@ type FooterColProps = {
 const FooterCol = ({ title, children }: FooterColProps) => {
   return (
     <div className='flex flex-col gap-5'>
-      <h4 className='bold-18 whitespace-nowrap'>{title}</h4>
+      <h4 className='regular-18 whitespace-nowrap'>{title}</h4>
       {children}
     </div>
   );
